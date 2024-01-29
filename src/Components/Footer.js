d0a8f9df6003985f22775382783487adc5f0a417
melindamackenzie <mem167@pitt.edu>
2024-01-28 20:16:05 -0500

    verified footer working

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,6 +2,12 @@ import React from "react";
 
 function Footer ()  {
 
+    return (
+        <div>
+            <hr></hr>
+            <h1 className = "footer"> Thank you for visiting!</h1>
+        </div>
+    )
 }
 
 export default Footer;
